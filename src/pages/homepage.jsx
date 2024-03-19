99dbc77878662f4c595e86d7f3f4fbb65c0d9606
teyvi <44750503+teyvi@users.noreply.github.com>
2024-03-19 09:41:03 +0000

    added footer to homepage

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from '../components/navbar';
+import Footer from '../components/footer';
 
 const Homepage = () => {
   return (
     <div>
       <Navbar/>
+      <Footer/>
+      
     </div>
   )
 }
