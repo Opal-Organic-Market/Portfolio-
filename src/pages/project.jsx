c8cf94a5204b33df1d34654f7a87d1a72df0f313
JoanaQuadzie <joanaq03@mail.com>
2024-03-22 05:37:57 -0700

    added an h1

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,43 +1,30 @@
 import React from "react";
 import Navbar from "../components/navbar";
+import Projectimage2 from "../asset/images/projectimage2.jpg";
 
 const Project = () => {
   return (
     <section>
-      <h2>KNOW MORE ABOUT OUR PROJECTS</h2>
-      <a
+      <Navbar />
+      <h1 className="text-black text-center py-7 underline-offset-1 font-bold text-2xl ">FIND OUR EXCITING PROJECTS</h1>
+      <a className=""
         href="#"
         class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
       >
-        <img
-          class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-          src="/docs/images/blog/image-4.jpg"
-          alt=""
-        />
-        <div class="flex flex-col justify-between p-4 leading-normal">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
+        <a href="https://unsplash.com/photos/baby-clinging-on-back-1YnBzhJISg4">
+        <img className="object-cover w-full rounded-t-lg h-96  md:w-48  " src="../asset/images/projectimage2.jpg" alt=""></img>
+       </a>
+        <div className=" flex-row justify-between pl-7 mx-2.5 leading-normal">
+          <h5 className="mb-2 mr-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Opal Organic Market
           </h5>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          In today's fast-paced world, convenience is king, and what could be more convenient than having access to a wide array of products right at your fingertips? Our team has been hard at work behind the scenes, crafting an online shopping experience that will revolutionize the way you shop.
           </p>
         </div>
 
-        <img
-          class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-          src="/docs/images/blog/image-4.jpg"
-          alt=""
-        />
-        <div class="flex flex-col justify-between p-4 leading-normal">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-        </div>
+        
+
       </a>
     </section>
   );
