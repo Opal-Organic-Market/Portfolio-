a1f3b1b2744fca522b7e7206d0791ac70282d5d6
JoanaQuadzie <joanaq03@gmail.com>
2024-03-22 12:21:10 +0000

    redesigned card

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -6,7 +6,49 @@ const Project = () => {
   return (
     <section>
       <Navbar />
-      <h1 className="text-black text-center py-7 underline-offset-1 font-bold text-2xl ">FIND OUR EXCITING PROJECTS</h1>
+      <div>
+      <h1 className="text-black text-center font-bold text-2xl ">FIND OUR EXCITING PROJECTS</h1><hr />
+        <div className="container mx-auto mt-5 grid grid-cols-2 gap-5">
+
+          <div>
+            <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg m-2" src={Projectimage2} alt="" />
+              <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+              </div>
+            </a>
+          </div>
+          <div>
+            <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={Projectimage2} alt="" />
+              <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+              </div>
+            </a>
+          </div>
+          <div>
+            <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={Projectimage2} alt="" />
+              <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+              </div>
+            </a>
+          </div>
+          <div>
+            <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={Projectimage2} alt="" />
+              <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* <h1 className="text-black text-center py-7 underline-offset-1 font-bold text-2xl ">FIND OUR EXCITING PROJECTS</h1>
       <a className=""
         href="#"
         class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
@@ -23,7 +65,6 @@ const Project = () => {
           </p>
         </div>
 
-<<<<<<< HEAD
 
         <img
           class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
@@ -39,11 +80,9 @@ const Project = () => {
             far, in reverse chronological order.
           </p>
         </div>
-=======
         
 
->>>>>>> c8cf94a5204b33df1d34654f7a87d1a72df0f313
-      </a>
+      </a> */}
     </section>
   );
 };
