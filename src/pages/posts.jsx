2d75d745847dcefe9aaf47f208b5e3c1a3590ef8
teyvi <44750503+teyvi@users.noreply.github.com>
2024-03-27 18:56:08 +0000

    fixes

--- src/pages/posts.jsx
+++ src/pages/posts.jsx
@@ -3,17 +3,14 @@ import Navbar from "../components/navbar";
 import { Card } from "flowbite-react";
 import Projectimage3 from "../asset/images/projectimage3.jpg";
 
-
 const Posts = () => {
   return (
-   <div>
     <div>
-         <Navbar />
-    
-    
-  </div>
-    
-</div>
+      <div>
+        <Navbar />
+      </div>
+
+    </div>
   );
 };
 export default Posts;
