c665d1f05b5ea03da573e90dcbabf50be848f064
JoanaQuadzie <joanaq03@gmail.com>
2024-03-27 19:59:47 +0000

    changed bg color

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,50 +1,50 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
 import Groupphoto from "../asset/images/groupphoto.jpg";
-import axios from "axios";
 
 const Project = () => {
-  const [project, setProject] = useState()
-  const url = (`https://portfolio-api-nmrs.onrender.com`)
+  const [projects, setProjects] = useState(null);
 
-useEffect (() => {
-  async function displayOneProject() {
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  const fetchProjects = async () => {
     try {
-       const project = await axios.get(url)
-       const projectname = project.data
-       console.log(project)
-
+      const response = await fetch("https://portfolio-api-nmrs.onrender.com/getprojects");
+      if (!response.ok) {
+        throw new Error('Failed to fetch projects');
+      }
+      const data = await response.json();
+      setProjects(data);
     } catch (error) {
-      console.log(error)
-      
+      console.error("Error fetching projects:", error);
     }
-    }
-    displayOneProject()
-})
+  };
 
+  if (!projects) {
+    return <h1>Please Wait</h1>;
+  }
 
   return (
-
     <>
       <Navbar />
-      <div className="bg-red-950 mx-auto mt-10">
-        <div className="m-auto w-2/5">
-
-          <a href="#" class="flex items-center bg-white border border-gray-200  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <img class="object-cover h-80 md:w-48 md:rounded-none md:rounded-s-lg" src={Groupphoto} alt="" />
-            <div class="flex flex-col justify-between p-4">
-              <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Opal Portfolio Project</h5>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A team of four junior developers embarked on building a challenging portfolio website, which proved to be both educational and rewarding. Despite initial obstacles, their collaboration, dedication, and perseverance led to the successful completion of the project</p>
+      <div className="bg-slate-700 h-dvh">
+        <div className="m-auto w-2/5 ">
+          {projects.map((project) => (
+            <div key={project._id}>
+              <a href="#" className=" flex items-center bg-gray-300 border hover:bg-gray-100 dark:border-gray-700  dark:hover:bg-gray-700">
+                <img className="h-80 md:w-48 md:rounded-none md:rounded-lg" src={project.image} alt="project" />
+                <div className="flex flex-col justify-between p-4">
+                  <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white mt-10 underline decoration-wavy">{project.title}</h5>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 ">{project.description}</p>
+                </div>
+              </a>
             </div>
-          </a>
-          
+          ))}
         </div>
-      </div>
-
-
-   
+        </div>
     </>
-
   );
 };
 
